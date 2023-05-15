fadd29ee1db9aac60552aad7f124e389191ddc77
Lando Calrissian <rebornwebjedi@yahoo.com>
2023-05-14 20:34:59 -0700

    Update Layout.tsx
    Link to my site

--- app/frontend/src/pages/layout/Layout.tsx
+++ app/frontend/src/pages/layout/Layout.tsx
@@ -35,7 +35,7 @@ const Layout = () => {
         <div className={styles.layout}>
             <header className={styles.header} role={"banner"}>
                 <div className={styles.headerContainer}>
-                    <Link to="https://dataaipdfchat.azurewebsites.net/" target={"_blank"} className={styles.headerTitleContainer}>
+                    <Link to="https://rcschatapp.azurewebsites.net/" target={"_blank"} className={styles.headerTitleContainer}>
                         <h3 className={styles.headerTitle}>Chat and Ask</h3>
                     </Link>
                     <nav>
